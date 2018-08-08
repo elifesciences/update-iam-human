b8dbf53bed34907af1c26d48eeb40a3c984b677f
Luke <lsh-0@users.noreply.github.com>
2018-08-08 17:13:00 +0930

    added pylint, pyflakes, pytest, test and lint scripts

---
+++ lint.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e
+
+echo "[-] .lint.sh"
+
+source venv/bin/activate
+
+# pylint likes to lint them
+find src/ -name '*.py[c|~]' -delete
+find src/ -regex "\(.*__pycache__.*\|*.py[co]\)" -delete
+
+echo "pyflakes"
+pyflakes ./src/
+# disabled until pylint supports Python 3.6
+# https://github.com/PyCQA/pylint/issues/1113
+
+echo "pylint"
+pylint -E ./src/ --disable=E1103 2> /dev/null
+# specific warnings we're interested in, comma separated with no spaces
+# presence of these warnings are a failure
+pylint ./src/ --disable=all --reports=n --score=n \
+    --enable=redefined-builtin
+
+echo "[✓] .lint.sh"
---
+++ src/tests/test_main.py
@@ -0,0 +1,7 @@
+from src.main import ensure
+import pytest
+
+def test_ensure():
+    ensure(1 == 1, "working")
+    with pytest.raises(AssertionError):
+        ensure(1 == 2, "not working")    
